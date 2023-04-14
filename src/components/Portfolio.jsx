cbd09d35c8b809c80b8c70a90c46d0111b58e32d
MateuszEM88 <mateuszem1988@gmail.com>
2023-04-14 14:45:52 +0200

    first refactoring

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
-import Card from "./Card";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import projects from "../data";
+import arrow from "./img/right-arrow.png";
 
 const Portfolio = () => {
   useEffect(() => {
@@ -14,38 +15,46 @@ const Portfolio = () => {
         <pre>&lt;projects&gt;</pre>
       </h1>
       <div className="flex justify-center items-center flex-wrap w-full">
-        <div
-          data-aos="fade-right"
-          data-aos-easing="ease-in-sine"
-          data-aos-duration="500"
-          className="w-full h-full"
-        >
-          <Card />
-        </div>
-        <div
-          data-aos="fade-right"
-          data-aos-easing="ease-in-sine"
-          data-aos-duration="500"
-          className="w-full h-full"
-        >
-          <Card />
-        </div>
-        <div
-          data-aos="fade-right"
-          data-aos-easing="ease-in-sine"
-          data-aos-duration="500"
-          className="w-full h-full"
-        >
-          <Card />
-        </div>
-        <div
-          data-aos="fade-right"
-          data-aos-easing="ease-in-sine"
-          data-aos-duration="500"
-          className="w-full h-full"
-        >
-          <Card />
-        </div>
+        {projects.map((project) => {
+          return (
+            <div
+              key={project.id}
+              data-aos="fade-right"
+              data-aos-easing="ease-in-sine"
+              data-aos-duration="500"
+              className="w-full h-full"
+            >
+              <div className="group w-full border-t-2 md:h-56 py-4 mx-2 flex flex-row max-md:flex-col justify-around  text-primaryText dark:text-darkText ">
+                <img
+                  src={project.img}
+                  alt="project view"
+                  className="flex w-1/4 max-md:w-full max-md:px-4 items-center justify-center px-4"
+                />
+
+                <div className="flex flex-col items-center max-md:w-full md:w-1/2">
+                  <h1 className="mb-4 p-2 w-full flex font-semibold max-md:justify-center max-sm:text-xl text-3xl">
+                    {project.name}
+                  </h1>
+                  <div className="flex w-full max-md:invisible max-md:absolute flex-wrap ">
+                    {project.tech.map((tech, index) => {
+                      return (
+                        <button
+                          key={index}
+                          className="m-1 px-4 color-indigo-500 rounded-full text-indigo-500 "
+                        >
+                          {tech}
+                        </button>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div className="flex flex-row text-xl max-md:w-full w-1/4 font-semibold items-center justify-center">
+                  <img src={arrow} alt="link" className="w-12 h-12" />
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,11 +1,18 @@
 import blog from "./components/img/blog.png";
+import portfolio from "./components/img/portfolio.jpg";
 
 const projects = [
   {
     id: 1,
     name: "My personal blog.",
-    tech: ["html", "Css", "Tailwind.css", "React", "Next.js", "GraphQL"],
+    tech: ["Html", "Css", "Tailwind.css", "React", "Next.js", "GraphQL"],
     img: `${blog}`,
+  },
+  {
+    id: 2,
+    name: "My portfolio site.",
+    tech: ["Html", "Css", "Tailwind.css", "React"],
+    img: `${portfolio}`,
   },
 ];
 
